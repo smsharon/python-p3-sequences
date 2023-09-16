e4f7dedfced24ad7d41ad2552a89769647db8295
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-09-16 10:41:33 -0400

    assignment completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+        return
+    elif length == 1:
+        print("[0]") 
+        return    
+    a, b = 0, 1  
+    fib_sequence = [0]
+
+    for i in range(1, length):
+        fib_sequence.append(b)
+        a, b = b, a + b  
+
+    print(f"[{', '.join(map(str, fib_sequence))}]") 
